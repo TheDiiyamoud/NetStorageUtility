0deb7230b6b473f1471deae3b28675309c1db2bd
Danial Yazdani Moghadam <danialyazdanimoghadam419@gmail.com>
2024-06-21 20:48:56 +0330

    updated log-in panel

--- client/src/main/java/view/registration/LoginPanel.java
+++ client/src/main/java/view/registration/LoginPanel.java
@@ -1,7 +1,9 @@
 package view.registration;
 
 import javax.swing.*;
+import javax.swing.border.TitledBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 
 public class LoginPanel extends JPanel {
     private static LoginPanel instance;
@@ -11,15 +13,41 @@ public class LoginPanel extends JPanel {
     private JLabel errorDisplayLabel;
 
     private LoginPanel() {
-        this.setLayout(new GridLayout());
-        this.usernameField = new JTextField();
-        this.passwordField = new JPasswordField();
+        this.setLayout(new GridBagLayout());
+        GridBagConstraints gridBagConstraints = new GridBagConstraints();
+        this.usernameField = new JTextField("", 20);
+        this.usernameField.setBorder(new TitledBorder("Username"));
+        this.passwordField = new JPasswordField("", 20);
+        this.passwordField.setBorder(new TitledBorder("Password"));
         this.proceedButton = new JButton("Enter");
         this.errorDisplayLabel = new JLabel();
+        this.errorDisplayLabel.setForeground(Color.red);
+        gridBagConstraints.fill = GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        add(usernameField, gridBagConstraints);
+        gridBagConstraints.fill = GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 1;
+        add(passwordField, gridBagConstraints);
+        gridBagConstraints.fill = GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 2;
+        add(proceedButton, gridBagConstraints);
+        gridBagConstraints.fill = GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 3;
+        gridBagConstraints.gridwidth = 4;
+        add(errorDisplayLabel, gridBagConstraints);
+        buttonActionListener();
     }
 
 
-
+    void buttonActionListener() {
+        proceedButton.addActionListener((ActionEvent e) -> {
+            errorDisplayLabel.setText("Hola");
+        });
+    }
     public static LoginPanel getInstance() {
         if (instance == null) {
             instance = new LoginPanel();
