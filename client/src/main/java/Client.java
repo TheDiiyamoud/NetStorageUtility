d505bed34f808c8eab5c6811e6bce01f7ef63d4f
Danial Yazdani Moghadam <danialyazdanimoghadam419@gmail.com>
2024-06-25 13:24:33 +0330

    initial single-threaded file transferring with udp implemented

--- client/src/main/java/Client.java
+++ client/src/main/java/Client.java
@@ -1,11 +1,33 @@
 import view.MainFrame;
 
 import javax.swing.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 public class Client {
     public static void main(String[] args) {
-        while (true) {
-            System.out.println("Hello");
+        try {
+            File file = new File("/home/dii/Desktop/Origin/filet.zip");
+            byte[] buffer = new byte[1024];
+            FileInputStream fileInputStream = new FileInputStream(file);
+            DatagramSocket datagramSocket= new DatagramSocket();
+            InetAddress ip = InetAddress.getByName("127.0.0.1");
+            int bytesRead;
+            // bytesRead is the count of the bytes existing in the array, -1 otherwise
+            while ((bytesRead = fileInputStream.read(buffer)) != -1) {
+                DatagramPacket datagramPacket = new DatagramPacket(buffer, bytesRead, ip, 3000);
+                datagramSocket.send(datagramPacket);
+            }
+            datagramSocket.close();
+            fileInputStream.close();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
--- server/src/main/java/Server.java
+++ server/src/main/java/Server.java
@@ -1,7 +1,41 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
 public class Server {
     public static void main(String[] args) {
-        for (;;) {
-            System.out.println("Hi");
+        FileOutputStream fileOutputStream = null;
+        DatagramSocket datagramSocket = null;
+        try {
+            datagramSocket = new DatagramSocket(3000);
+            byte[] buffer = new byte[1024];
+            File file = new File("/home/dii/Desktop/Destination/filet.zip");
+            fileOutputStream = new FileOutputStream(file);
+            while (true) {
+                DatagramPacket datagramPacket = new DatagramPacket(buffer, 1024);
+                datagramSocket.receive(datagramPacket);
+                fileOutputStream.write(datagramPacket.getData(), 0, datagramPacket.getLength());
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (fileOutputStream != null) {
+                    fileOutputStream.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (datagramSocket != null) {
+                datagramSocket.close();
+            }
         }
     }
 }
+
+
