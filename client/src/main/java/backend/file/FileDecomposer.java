971d02e0d1bce731ff1f8a66ae9cbc2ebe665db9
Danial Yazdani Moghadam <danialyazdanimoghadam419@gmail.com>
2024-07-01 11:37:30 +0330

    updated file decomposing class

--- client/src/main/java/backend/file/FileDecomposer.java
+++ client/src/main/java/backend/file/FileDecomposer.java
@@ -6,24 +6,34 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class FileDecomposer implements Runnable{
-    String filePath;
-
+    private final String filePath;
+    private final long fileSize;
+    private final int numChunks;
     public FileDecomposer(String filePath) {
          this.filePath = filePath;
+         fileSize = new File(filePath).length();
+         numChunks = calculateNumChunks();
+    }
+
+    private int calculateNumChunks() {
+        int count;
+        if (fileSize <= 100 * 1024 * 1024) {
+            count = 5; // Divide into 5 parts for files <= 100 MB
+        } else if (fileSize <= 2L * 1024 * 1024 * 1024) {
+            count = 10; // Divide into 10 parts for files <= 2 GB
+        } else {
+            count = 20; // Divide into 20 parts for larger files
+        }
+        return count;
+
+    }
+
+    public int getNumChunks() {
+        return numChunks;
     }
 
     @Override
     public void run() {
-        long fileSize = new File(filePath).length();
-        int numChunks;
-
-        if (fileSize <= 100 * 1024 * 1024) {
-            numChunks = 5; // Divide into 5 parts for files <= 100 MB
-        } else if (fileSize <= 2L * 1024 * 1024 * 1024) {
-            numChunks = 10; // Divide into 10 parts for files <= 2 GB
-        } else {
-            numChunks = 20; // Divide into 20 parts for larger files
-        }
 
         int chunkSize = (int) Math.ceil((double) fileSize / numChunks);
 
@@ -38,7 +48,9 @@ public class FileDecomposer implements Runnable{
                 try (FileOutputStream fos = new FileOutputStream(chunkFileName)) {
                     fos.write(buffer, 0, bytesRead);
                 }
-                chunkNumber++;
+                if (chunkNumber < numChunks) {
+                    chunkNumber++;
+                }
             }
         } catch (
                 IOException e) {
